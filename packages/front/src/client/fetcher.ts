bd4b557f490cb4c180cdcca029aeb83c68f2ba66
peacepiece7 <peacepiece7@naver.com>
2023-12-28 09:55:00 +0900

    chore : vercel production 환경 변수 추가

--- packages/front/src/client/fetcher.ts
+++ packages/front/src/client/fetcher.ts
@@ -2,7 +2,7 @@
 export async function fetcher<T>(url: string, init?: RequestInit): Promise<T> {
   // * 개발, 테스트 환경에서는 localhost:3000 으로 접속합니다!
   // prettier-ignore
-  const baseURL = process.env.NODE_ENV === 'development'  || process.env.NODE_ENV === 'test' ? 'http://localhost:3000' : 'https://mywebsite.com'
+  const baseURL = process.env.NODE_ENV === 'development'  || process.env.NODE_ENV === 'test' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 
   return fetch(`${baseURL}${url}`, init).then((res) => {
     if (!res.ok) {
