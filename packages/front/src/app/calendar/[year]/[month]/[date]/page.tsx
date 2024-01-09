0ff59c2e8f8eb0553eb249dbbd0031a3643d5b3f
peacepiece7 <peacepiece7@naver.com>
2024-01-09 16:59:47 +0900

    feat(front) : 캘린더 not-found 페이지 추가 및 관련 로직 추가

--- packages/front/src/app/calendar/[year]/[month]/[date]/page.tsx
+++ packages/front/src/app/calendar/[year]/[month]/[date]/page.tsx
@@ -1,7 +1,8 @@
 import { getServerSession } from 'next-auth'
 import CalendarContainer from './_container/CalendarContainer'
 import { authOptions } from '@/service/nextAuth'
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
+import { dayManager } from '@/util/dayManager'
 
 export interface CalendarPageProps {
   params: {
@@ -12,13 +13,24 @@ export interface CalendarPageProps {
 }
 
 export default async function CalendarPage(props: CalendarPageProps) {
+  const { year, month, date } = props.params
   const session = await getServerSession(authOptions)
 
   if (!session) return redirect('/auth/signin')
+  if (!checkIsValidDate(`${year}-${month}-${date}`)) return notFound()
 
   return (
     <main>
       <CalendarContainer {...props.params} user={session.user} />
     </main>
   )
+}
+
+/**
+ * @description YYYY-MM-DD 형식으로 주어진 값이 유효한지 검사합니다.
+ * */
+function checkIsValidDate(date: string) {
+  const validDate = dayManager.dayToDefaultFormat(date)
+  if (validDate !== date) return false
+  return true
 }
